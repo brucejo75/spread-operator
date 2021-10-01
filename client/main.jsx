0e4de790c4d59f071da63f68704457395d9126a4
Bruce Johnson <brucejo.wk@outlook.com>
2021-10-01 07:55:28 -0700

    Initial commit

---
+++ client/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Meteor } from 'meteor/meteor';
+import { render } from 'react-dom';
+import { App } from '/imports/ui/App';
+
+Meteor.startup(() => {
+  render(<App/>, document.getElementById('react-target'));
+  let nodes = document.querySelectorAll("p");
+  foobar(...nodes);
+});
+
+function foobar(...args) {
+  console.log(args.join());
+}
